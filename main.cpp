2c37557ddcab69d9bff737cec5a11c3bbaab7b12
chenh735 <chenh735@mail2.sysu.edu.cn>
2026-06-14 16:37:27 +0800

    fixed: 修复一些bug

--- main.cpp
+++ main.cpp
@@ -3,15 +3,36 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <cstring>
+#include <stdlib.h>
+#include <errno.h>
 #include <iostream>
 
 
 
 int main(int argc, char* argv[]){
+    long port = 8080;
+    if(argc == 2){
+        errno = 0;
+        char* end = nullptr;
+        port = strtol(argv[1], &end, 0);
+        if(errno !=0 || *end != '\0' || port < 0 || port > 65535){
+            perror("port 格式错误\n");
+            return;
+        }
+    }
+
+
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if(-1 == fd){
         perror("socket 创建失败\n");
         return 1;
+    }
+
+    int opt = 1;
+    if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt) == -1)){
+        close(fd);
+        perror("setsockopt 失败\n");
+        return;
     }
     
     sockaddr_in in_addr{};
@@ -41,6 +62,16 @@ int main(int argc, char* argv[]){
 
         char buf[1024];
         ssize_t n = recv(client_fd, buf, sizeof(buf) - 1, 0);
+        if(n == -1){
+            close(client_fd);
+            perror("revc 错误\n");
+            break;
+        }
+        if(n == 0){
+            close(client_fd);
+            perror("客户端关闭连接\n");
+            continue;
+        }
         if(n > 0){
             buf[n] = '\0';
             std::cout << buf << std::endl;
@@ -50,6 +81,7 @@ int main(int argc, char* argv[]){
             "HTTP/1.1 200 OK\r\n"
             "Content-Type: text/plain\r\n"
             "Content-Length: 12\r\n"
+            "Connect: close\r\n"
             "\r\n"
             "Hello World\n";
         
@@ -59,11 +91,17 @@ int main(int argc, char* argv[]){
             ssize_t n = send(client_fd, (void*)(response + send_total), total - send_total, 0);
             if(n == -1){
                 perror("send 失败");
+                close(client_fd);
                 break;
+            }
+            if(n == 0){
+                close(client_fd);
+                perror("客户端关闭连接");
+                continue;
             }
             send_total += n;
         }
-        
+        close(client_fd);
     }
     
     close(fd);
