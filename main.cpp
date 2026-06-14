e0309a6a57a1a08cb918b319ad07e96472809d52
chenh735 <chenh735@mail2.sysu.edu.cn>
2026-06-14 15:35:40 +0800

    feat:实现最小服务器

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+
+
+
+int main(int argc, char* argv[]){
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(-1 == fd){
+        perror("socket 创建失败\n");
+        return 1;
+    }
+    
+    sockaddr_in in_addr{};
+    in_addr.sin_family = AF_INET;
+    in_addr.sin_port = htons(8080);
+    in_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if(-1 == bind(fd, (sockaddr*)(&in_addr), sizeof(in_addr))){
+        perror("bind 创建失败\n");
+        close(fd);
+        return -1;
+    }
+
+    if(-1 == listen(fd, SOMAXCONN)){
+        perror("listen 失败\n");
+        close(fd);
+        return -1;
+    }
+
+    while (true)
+    {
+        int client_fd = accept(fd, nullptr, NULL);
+        if(client_fd == -1){
+            sleep(1);
+            continue;
+        }
+
+        char buf[1024];
+        ssize_t n = recv(client_fd, buf, sizeof(buf) - 1, 0);
+        if(n > 0){
+            buf[n] = '\0';
+            std::cout << buf << std::endl;
+        }
+
+        const char* response = 
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 12\r\n"
+            "\r\n"
+            "Hello World\n";
+        
+        ssize_t total = strlen(response);
+        ssize_t send_total = 0;
+        while(send_total < total){
+            ssize_t n = send(client_fd, (void*)(response + send_total), total - send_total, 0);
+            if(n == -1){
+                perror("send 失败");
+                break;
+            }
+            send_total += n;
+        }
+        
+    }
+    
+    close(fd);
+    std::cout << "success" << std::endl;
+
+    return 0;
+}
